2f811411952a964f40a8b1d1d4a9786c26ccb2bd
Illya Moskvin <imoskvin@artic.edu>
2018-08-24 14:35:26 -0500

    Add stub import:artworks command
    This doesn't actually work because it targets search, and search cannot
    handle requests with an offset over 10,000. We need to implement the
    scroll API to make that work. See WEB-460.

---
+++ app/Behaviors/ImportsData.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Behaviors;
+
+trait ImportsData
+{
+
+    protected function fetch( $file, $decode = false ) {
+
+        if( !$contents = @file_get_contents( $file ) )
+        {
+            throw new \Exception('Fetch failed: ' . $file );
+        }
+
+        return $decode ? json_decode( $contents ) : $contents;
+
+    }
+
+    protected function query( $endpoint, $page = 1, $limit = 500 )
+    {
+        $url = env('API_URL') . '/' . $endpoint . '?page=' . $page . '&limit=' . $limit . '&fields=id,title,timestamp';
+
+        $this->info( 'Querying: ' . $url );
+
+        return $this->fetch( $url, true );
+
+    }
+
+    protected function import( $model, $endpoint, $current = 1 )
+    {
+
+        // Query for the first page + get page count
+        $json = $this->query( $endpoint, $current );
+
+        // Assumes the dataservice has standardized pagination
+        $pages = $json->pagination->total_pages;
+        // $total = $json->pagination->total;
+
+        // $bar = $this->output->createProgressBar($pages);
+
+        while( $current <= $pages )
+        {
+            foreach( $json->data as $datum )
+            {
+                $item = $this->save( $datum, $model );
+
+
+                $this->info('Imported #' . $item->id . ' - ' . $item->title);
+
+            }
+
+            $current++;
+
+            // $bar->advance();
+
+            $json = $this->query( $endpoint, $current );
+
+        }
+
+        // $bar->finish();
+
+    }
+
+}
---
+++ app/Console/Commands/ImportArtworks.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Behaviors\ImportsData;
+use App\Artwork;
+use Carbon\Carbon;
+
+class ImportArtworks extends AbstractCommand
+{
+
+    use ImportsData;
+
+    protected $signature = 'import:artworks';
+
+    protected $description = "Imports artworks from the data aggregator";
+
+    public function handle()
+    {
+
+        // TODO: Refactor ImportsData to use http_build_query($query)?
+        $query = [
+            'resources' => 'artworks',
+            'size' => 500,
+            'query' => [
+                // TODO: Add timestamp query here? Or last_modified_at
+            ],
+            'fields' => [
+                'id',
+                'title',
+            ],
+        ];
+
+        $this->import(Artwork::class, 'artworks/search');
+
+    }
+
+    protected function save( $datum, $model )
+    {
+
+        $artwork = $model::findOrNew($datum->id);
+
+        $artwork->id = $datum->id;
+        $artwork->title = $datum->title;
+        $artwork->indexed_at = new Carbon( $datum->timestamp );
+        $artwork->imported_at = new Carbon();
+
+        $artwork->save();
+
+        return $artwork;
+
+    }
+
+}
