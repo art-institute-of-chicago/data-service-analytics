a9f171ed3a7b08c8297772223447318e74331496
nikhiltri <ntrivedi@artic.edu>
2019-06-03 16:53:28 -0500

    Provide starting point for all import by artworks

--- app/Console/Commands/ImportAnalyticsForArtworksAll.php
+++ app/Console/Commands/ImportAnalyticsForArtworksAll.php
@@ -23,13 +23,15 @@ use Google_Service_AnalyticsReporting_GetReportsRequest as GetReportsRequest;
 class ImportAnalyticsForArtworksAll extends AbstractCommand
 {
 
-    protected $signature = 'import:analytics-for-artworks-all';
+    protected $signature = 'import:analytics-for-artworks-all
+                            {start-from : Artwork ID start sequentially start from}';
 
     protected $description = 'Imports analytics from Google all artworks';
 
     public function handle()
     {
-        Artwork::chunk(5, function ($artworks) {
+        $startFrom = $this->argument('start-from') ?? 0;
+        Artwork::where('id', '>=', $startFrom)->chunk(5, function ($artworks) {
             $this->call('import:analytics-for-artwork', [ 'artworks' => implode($artworks->pluck('id')->all(), ',') ]);
         });
     }
